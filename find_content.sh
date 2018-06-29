542fa58a7a9c762c5c272129828e4435d7b9af2f
Leonardo Leite <leonardofl87@gmail.com>
2018-06-29 08:15:22 -0300

    egrep -rin --color pattern *py

--- find_content.sh
+++ find_content.sh
@@ -3,4 +3,6 @@ find . -name '*.py' | xargs grep -i -n -H --color pattern
 # or
 egrep -rin --color pattern
 # rin = recursive, ignore-case, and number line
+# or to limit the search scope:
+egrep -rin --color pattern *py
 
