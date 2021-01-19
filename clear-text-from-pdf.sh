ade80648e758270a3d0feaa0478eaa2a62016fe6
leonardofl <leonardofl87@gmail.com>
2021-01-19 20:43:40 -0300

    clear pdf

--- clear-text-from-pdf.sh
+++ clear-text-from-pdf.sh
@@ -1,2 +1,2 @@
-# copia e cola conteúdo do PDF em text.txt e aí roda o script
+# copia e cola conteúdo do PDF em text.txt (deixando uma linha em branco entre os parágrafos) e aí roda o script
 cat text.txt | tr '\n' '|' | sed -e 's/-|//g' -e 's/||/\n\n/g' -e 's/|/ /g'
